e9cbfbfac3b1307e53b18ad02eb22df4cba66fe7
Daniel Kiss <dkiss@phase2technology.com>
2018-10-23 16:03:32 -0700

    add debug  alias

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -34,6 +34,8 @@ class ArticleController extends AbstractController
       'some comment3',
     ];
 
+    dump($slug, $this);
+
     return $this->render('article/show.html.twig', [
         'title' => ucwords(str_replace('-',' ', $slug)),
         'comments' => $comments,
